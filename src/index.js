f4ad768861672349d1da5b63da31c348d6f02fc1
Alim Bolar <alim@fourplusmedia.com>
2022-03-01 13:13:56 +0800

    finalised desktop version

--- src/index.js
+++ src/index.js
@@ -1,20 +1,64 @@
 import "./style.scss";
 
-console.log(document.querySelector("header").clientHeight);
+// LOGO
 
-document.querySelector(".menu").addEventListener("click", function () {
-  document.querySelector(".nav").classList.toggle("hidden");
+const logo = document.querySelector(".logo");
+
+logo.addEventListener("click", function () {
+  hero.scrollIntoView(false);
 });
 
-const knowMoreButton = document.querySelector(".know-more");
+// MENU
+
+const menu = document.querySelector(".menu");
+const headerNav = document.querySelector(".header__nav");
+
+menu.addEventListener("click", function () {
+  headerNav.classList.toggle("hidden");
+});
+
+// BUTTON
+
+const knowMoreButton = document.querySelector(".cta.know-more");
+const knowMoreMenu = document.querySelector(".nav__item.know-more");
+const homeMenu = document.querySelector(".nav__item.home");
 const profiles = document.querySelector(".profiles");
 const hero = document.querySelector(".hero");
-const logo = document.querySelector(".logo");
+console.log(homeMenu);
 
 knowMoreButton.addEventListener("click", function () {
   profiles.scrollIntoView();
 });
 
-logo.addEventListener("click", function () {
+knowMoreMenu.addEventListener("click", function () {
+  headerNav.classList.toggle("hidden");
+  profiles.scrollIntoView();
+});
+homeMenu.addEventListener("click", function () {
+  headerNav.classList.toggle("hidden");
   hero.scrollIntoView(false);
 });
+
+// PROFILES NAV
+
+const profilesNav = document.querySelector(".profiles__nav");
+console.log(profilesNav);
+
+profilesNav.addEventListener("click", goToProfile);
+
+const profilesContainer = document.querySelector(".profiles__container");
+
+const slideWidth = profilesContainer.clientWidth;
+
+console.log(profilesContainer);
+
+function goToProfile(e) {
+  e.preventDefault();
+
+  const target = e.target;
+  if (!target.closest(".nav__item")) return;
+  const slide = +target.closest(".nav__item").getAttribute("data-slide");
+
+  profilesContainer.style.transform = `translateX(${-slide * slideWidth}px)`;
+  profilesContainer.style.transition = "transform 1s";
+}
