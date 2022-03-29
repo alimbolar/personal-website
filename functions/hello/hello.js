d6458fe86800dafe1cef52d1737bdcd4d882ac6a
Alim Bolar <alimbolar@gmail.com>
2022-03-29 10:02:03 +0800

    minor update

--- functions/hello/hello.js
+++ functions/hello/hello.js
@@ -1,7 +1,4 @@
 import fetch from 'node-fetch';
-// https://answers.netlify.com/t/to-use-import-or-require-for-importing-node-fetch-for-netlify-serverless-functions-both-give-errors/50068/5
-
-// const fetch = require('node-fetch');
 
 exports.handler = async function(event, context){
 
