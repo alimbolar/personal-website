146a2816a84b4300db1aa5f39b06c4d02716f14e
Alim Bolar <alimbolar@gmail.com>
2022-03-29 09:36:36 +0800

    testing json output

--- functions/hello/hello.js
+++ functions/hello/hello.js
@@ -1,7 +1,12 @@
 exports.handler = async function(event, context){
 
+    const response = fetch('https://youandeyemag.com/wp-json/wp/v2/posts');
+
+    const data = response.json();
+
     return {
         statusCode:200,
-        body:JSON.stringify({message:"Hello World"})
+        body:JSON.stringify({message:"Hello World"}),
+        data
     }
 }
