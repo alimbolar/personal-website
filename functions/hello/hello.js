f374b6d9f494527670ee323b5751669befcab82b
Alim Bolar <alimbolar@gmail.com>
2022-03-29 09:46:33 +0800

    testing fetch

--- functions/hello/hello.js
+++ functions/hello/hello.js
@@ -1,6 +1,11 @@
+import fetch from 'node-fetch';
+// https://answers.netlify.com/t/to-use-import-or-require-for-importing-node-fetch-for-netlify-serverless-functions-both-give-errors/50068/5
+
+// const fetch = require('node-fetch');
+
 exports.handler = async function(event, context){
 
-    const response = fetch('https://youandeyemag.com/wp-json/wp/v2/posts');
+    const response = await fetch('https://youandeyemag.com/wp-json/wp/v2/posts');
 
     const data = response.json();
 
