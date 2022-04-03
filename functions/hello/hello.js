7c6b86f36c833441089d2efbbac8d31c09f95336
Alim Bolar <alim@fourplusmedia.com>
2022-04-03 13:18:55 +0800

    testing

--- functions/hello/hello.js
+++ functions/hello/hello.js
@@ -1,4 +1,4 @@
-import fetch from "node-fetch";
+// import fetch from "node-fetch";
 
 exports.handler = async function (event, context) {
   // const response = await fetch('https://youandeyemag.com/wp-json/wp/v2/posts');
