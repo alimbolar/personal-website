62fff4ddba1d85682f417f443ad63e9d96fdb65a
Alim Bolar <alim@fourplusmedia.com>
2022-04-03 13:34:23 +0800

    testing

--- functions/hello/hello.js
+++ functions/hello/hello.js
@@ -4,11 +4,11 @@ import fetch from "node-fetch";
 exports.handler = async function (event, context) {
   const response = await fetch("https://youandeyemag.com/wp-json/wp/v2/posts");
 
-  const data = JSON.stringify(response.json());
+  const data = await response.json();
 
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "Hello World" }),
-    data,
+    data: JSON.stringify(data),
   };
 };
